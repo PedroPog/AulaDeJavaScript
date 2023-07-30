f4515867148513aec1ae06385c1b340e2fde4b5d
Pedro Vieira <pevieiraf@gmail.com>
2023-07-30 18:10:46 -0300

    E

--- teste/script.js
+++ teste/script.js
@@ -1,19 +1,27 @@
-var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    slidesPerView: "1",
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 4,
-      slideShadows: false
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 2
-      }
+const carouselContainer = document.getElementById('carousel-container');
+const images = document.querySelectorAll('.imagem');
+
+let idx = 0;
+
+function highlightImage(index) {
+    images.forEach((image, i) => {
+        if (i === index) {
+            image.classList.add('highlight');
+        } else {
+            image.classList.remove('highlight');
+        }
+    });
+}
+
+function carousel() {
+    idx++;
+    if (idx > images.length - 1) {
+        idx = 0;
     }
-  });
+    highlightImage(idx);
+    //carouselContainer.style.transform = `translateX(${-idx * 10}vw)`;
+}
+
+setInterval(carousel, 1800);
+
+
